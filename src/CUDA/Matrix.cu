5623c24c8315d79490e7438eacd86dc704e11d92
Ayush Mishra <ayushmishra.iit@gmail.com>
2018-05-05 23:50:06 +0530

    CUDA

--- src/CUDA/Matrix.cu
+++ src/CUDA/Matrix.cu
@@ -9,7 +9,7 @@ struct MatrixCOOUnsorted {
     double * val;
 };
 
-MatrixCOOUnsorted * readMatrix(string filename) {
+MatrixCOOUnsorted * readMatrixUnified(string filename) {
     std::ifstream fin(filename);
     int M, N, L;
     // Ignore headers and comments:
@@ -30,6 +30,51 @@ MatrixCOOUnsorted * readMatrix(string filename) {
     cudaMallocManaged(&matrix_coo->j, sizeof(int) * L);
     cudaMallocManaged(&matrix_coo->val, sizeof(double) * L);
 
+    int filled = 0;
+
+    vector< vector <pair<int, double> > > matrix_data(M);
+    for (int l = 0; l < L; l++) {
+        int m, n;
+        double data;
+        fin >> m >> n >> data;
+        matrix_data[m - 1].push_back({n - 1, data});
+    }
+    for(int i = 0; i < M; i++) {
+        sort(matrix_data[i].begin(), matrix_data[i].end());
+        for(auto tp : matrix_data[i]) {
+            matrix_coo->i[filled] = i;
+            matrix_coo->j[filled] = tp.first;
+            matrix_coo->val[filled] = tp.second;
+            filled++;
+        }
+    }
+
+    assert(filled == L);
+    fin.close();
+    cerr << "Read matrix from file: " << filename << endl;
+    return matrix_coo;
+}
+
+MatrixCOOUnsorted * readMatrixCPU(string filename) {
+    std::ifstream fin(filename);
+    int M, N, L;
+    // Ignore headers and comments:
+    while (fin.peek() == '%') fin.ignore(2048, '\n');
+    // Read defining parameters:
+    fin >> M >> N >> L;
+    // Read the data
+
+    MatrixCOOUnsorted * matrix_coo;
+    matrix_coo =  (MatrixCOOUnsorted *) malloc(sizeof(MatrixCOOUnsorted));
+
+    matrix_coo->rows = M;
+    matrix_coo->cols = N;
+    matrix_coo->nnz = L;
+
+    matrix_coo->i = malloc(sizeof(int) * L);
+    matrix_coo->j = malloc(sizeof(int) * L);
+    matrix_coo->val = malloc(sizeof(double) * L);
+    
     int filled = 0;
 
     vector< vector <pair<int, double> > > matrix_data(M);
