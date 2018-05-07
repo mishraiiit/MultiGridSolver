e8de66fc22b78def6d59ddfa3e3968b5c2578eb6
Ayush Mishra <ayushmishra.iit@gmail.com>
2018-05-07 17:53:29 +0530

    Random access sparse matrix __device__ functions getElementMatrixCSR/CSC

--- src/CUDA/Matrix.cu
+++ src/CUDA/Matrix.cu
@@ -444,3 +444,43 @@ MatrixCSC * readMatrixGPUMemoryCSC(string filename) {
 
     return matrix_csc;
 }
+
+__device__ double getElementMatrixCSR(MatrixCSR * matrix, int i, int j) {
+    int l = matrix->i[i];
+    int r = matrix->i[i + 1];
+    if(l == r) return 0.0;
+
+    while(l != r) {
+        int mid = (l + r) / 2;
+        if(matrix->j[mid] < j) {
+            l = mid + 1;
+        } else {
+            r = mid;
+        }
+    }
+
+    if(matrix->j[l] == j)
+        return matrix->val[l];
+    else
+        return 0.0;
+}
+
+__device__ double getElementMatrixCSC(MatrixCSC * matrix, int i, int j) {
+    int l = matrix->j[j];
+    int r = matrix->j[j + 1];
+    if(l == r) return 0.0;
+
+    while(l != r) {
+        int mid = (l + r) / 2;
+        if(matrix->i[mid] < i) {
+            l = mid + 1;
+        } else {
+            r = mid;
+        }
+    }
+
+    if(matrix->i[l] == i)
+        return matrix->val[l];
+    else
+        return 0.0;
+}
--- src/CUDA/main.cu
+++ src/CUDA/main.cu
@@ -11,17 +11,17 @@ __global__ void debugCOO(MatrixCOO * matrix) {
 
 __global__ void debugCSR(MatrixCSR * matrix) {
 	for(int i = 0; i < matrix->rows; i++) {
-		for(int j = matrix->i[i]; j < matrix->i[i + 1]; j++) {
-			printf("%d %d %lf\n", i, matrix->j[j], matrix->val[j]);
+		for(int j = 0; j < matrix->cols; j++) {
+			printf("%lf ", getElementMatrixCSR(matrix, i, j));
 		}
+		printf("\n");
 	}
 }
 
-__global__ void linebyline(MatrixCSR * matrix) {
-	int i = blockIdx.x * blockDim.x + threadIdx.x;
-	if(i == 5) {
-		for(int j = matrix->i[i]; j < matrix->i[i + 1]; j++) {
-			printf("%d %d %lf----", i, matrix->j[j], matrix->val[j]);
+__global__ void debugCSC(MatrixCSC * matrix) {
+	for(int i = 0; i < matrix->rows; i++) {
+		for(int j = 0; j < matrix->cols; j++) {
+			printf("%lf ", getElementMatrixCSC(matrix, i, j));
 		}
 		printf("\n");
 	}
@@ -29,27 +29,13 @@ __global__ void linebyline(MatrixCSR * matrix) {
 
 int main() {
 
-	auto tempCOO = readMatrixGPUMemoryCOO("../../matrices/SmallTestMatrix.mtx");
-	debugCOO <<<1,1>>> (tempCOO);
+	auto tempCSR = readMatrixGPUMemoryCSR("../../matrices/SmallTestMatrix.mtx");
+	debugCSR <<<1,1>>> (tempCSR);
 	cudaDeviceSynchronize();
 
-	auto tempCSC = readMatrixUnifiedMemoryCSC("../../matrices/SmallTestMatrix.mtx");
-	for(int i = 0; i < tempCSC->cols; i++) {
-		for(int j = tempCSC->j[i]; j < tempCSC->j[i + 1]; j++) {
-			printf("%d %d %lf\n", tempCSC->i[j], i, tempCSC->val[j]);
-		}
-	}
-	
-
-	// auto tempCSRCPU = readMatrixCPUMemoryCSR("../../matrices/SmallTestMatrix.mtx");
-	// auto tempCSR = readMatrixGPUMemoryCSR("../../matrices/SmallTestMatrix.mtx");
-
-
-	// // debugCSR <<<1,1>>> (tempCSR);
-	// cudaDeviceSynchronize();
-
-	// linebyline <<< (tempCSRCPU->rows + 1 - 1) / 1, 1 >>> (tempCSR);
-	// cudaDeviceSynchronize();
+	auto tempCSC = readMatrixGPUMemoryCSC("../../matrices/SmallTestMatrix.mtx");
+	debugCSC <<<1,1>>> (tempCSC);
+	cudaDeviceSynchronize();
 
 	return 0;
 }
