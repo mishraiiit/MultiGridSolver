97f113d41e55f6192b10a0eec9524da987b19f3e
Ayush Mishra <ayushmishra.iit@gmail.com>
2018-07-01 19:24:42 +0530

    Added FGMRES..

--- src/CPU_C++/compile_mkl.sh
+++ src/CPU_C++/compile_mkl.sh
@@ -1,2 +1,4 @@
-g++ -std=c++11 $1  -Wl,--start-group ${MKLROOT}/lib/intel64/libmkl_intel_ilp64.a ${MKLROOT}/lib/intel64/libmkl_sequential.a ${MKLROOT}/lib/intel64/libmkl_core.a -Wl,--end-group -lpthread -lm -ldl  -DMKL_ILP64 -m64 -I${MKLROOT}/include -I ../../lib
+export MKLROOT='/opt/intel/mkl'
+g++ -std=c++11 -msse4 -O3 $1  -Wl,--start-group ${MKLROOT}/lib/intel64/libmkl_intel_ilp64.a ${MKLROOT}/lib/intel64/libmkl_sequential.a ${MKLROOT}/lib/intel64/libmkl_core.a -Wl,--end-group -lpthread -lm -ldl  -DMKL_ILP64 -m64 -I${MKLROOT}/include -I ../../lib
+
 
